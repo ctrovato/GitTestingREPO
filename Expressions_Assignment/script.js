210e2e31a1b6507608b0c328571552b02315a0f2
ctrovato <carminetrovato@fullsail.edu>
2013-09-21 23:45:44 -0400

    Expressions Assignment
    Console.log the first part of the problem

--- Expressions_Assignment/script.js
+++ Expressions_Assignment/script.js
@@ -39,3 +39,4 @@ console.log("")
 /* __________________PROBLEM TWO: Industrial________________________________________
  This Problem calculates the price I will pay for the new 32GB iPhone 5s with a two year contract after sales tax.
  */
+console.log("Problem Two:  Industrial")
