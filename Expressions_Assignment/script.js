53994b399e4124868a31dd3a6f29deffa790d43d
ctrovato <carminetrovato@fullsail.edu>
2013-09-21 23:36:44 -0400

    Expressions Assignment
    console.log the first problem

---
+++ Expressions_Assignment/script.js
@@ -0,0 +1,24 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: CarmineTrovato
+ * Date: 9/20/13
+ * Time: 9:28 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+
+/*
+    Carmine Trovato
+    09/20/2013
+    Expressions Assignment
+    */
+
+
+
+
+/*
+_________________PROBLEM ONE: Personal____________________________________________
+This Problem calculates my average GPA over the past 7 semesters.
+*/
+
+console.log("Problem One:  Personal");
