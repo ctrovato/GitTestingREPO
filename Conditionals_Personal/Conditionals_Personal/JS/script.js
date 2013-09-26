4ecd6f66131aa7e10b315e57a0a565219e7ace7e
ctrovato <carminetrovato@fullsail.edu>
2013-09-26 19:29:01 -0400

    Conditionals: Personal
    Started Problem: Added Variables

---
+++ Conditionals_Personal/Conditionals_Personal/JS/script.js
@@ -0,0 +1,17 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: CarmineTrovato
+ * Date: 9/26/13
+ * Time: 7:28 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+
+/*   Carmine Trovato      09/24/2013        Conditionals Industry  */
+
+/* This Problem will find out if you will have enough money to buy a car without a loan */
+
+/* Users input the Variables to obtain the totals of how much money you have */
+var savings1=prompt("Enter Checking Account Total: ");
+var checking1=prompt("Enter Savings AccountTotal: ");
+var other1=prompt("Enter Other Assets Total: ");
