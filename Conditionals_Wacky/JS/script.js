6f2746393b8a9e20be36419ef1231c3a2d71167e
ctrovato <carminetrovato@fullsail.edu>
2013-09-27 19:40:58 -0400

    Conditionals Wacky
    console.log Title

--- Conditionals_Wacky/JS/script.js
+++ Conditionals_Wacky/JS/script.js
@@ -1,3 +1,5 @@
 //Carmine Trovato       09/24/2013       Conditionals Wacky
 
 /* This Problem Will find if we have enough Diplomas For Each Student */
+/* Console.log the Problem Name  */
+console.log("Do We Have Enough Diplomas??")
