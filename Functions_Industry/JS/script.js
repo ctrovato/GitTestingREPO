105c374007197499d54c7b6f3321993ad3bc4694
ctrovato <carminetrovato@fullsail.edu>
2013-09-28 05:44:55 -0400

    Functions: Industry
    Started the Assignment

---
+++ Functions_Industry/JS/script.js
@@ -0,0 +1,11 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: CarmineTrovato
+ * Date: 9/27/13
+ * Time: 4:53 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+//Carmine Trovato        09/27/2013           Functions: Industry
+
+/*This problem will calculate if a customer is eligible for a discount based on ones age */
