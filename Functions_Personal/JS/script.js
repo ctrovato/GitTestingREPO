3fb1025d02215234c813ec4ba40f2b61abbed06c
ctrovato <carminetrovato@fullsail.edu>
2013-09-28 00:06:47 -0400

    Functions: Personal
    Started the Assignment

---
+++ Functions_Personal/JS/script.js
@@ -0,0 +1,10 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: Carmine Trovato
+ * Date: 9/25/13
+ * Time: 6:56 PM
+ * To change this template use File | Settings | File Templates. */
+
+//Carmine Trovato         09/25/2013              Functions: Personal
+
+/* This Problem will calculate if your eligible for a discount based on your StudentID or the time day  */
