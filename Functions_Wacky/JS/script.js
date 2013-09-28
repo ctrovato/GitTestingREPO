2c3e05764340d07f429df7d18116723d7ec0a40a
ctrovato <carminetrovato@fullsail.edu>
2013-09-28 16:10:21 -0400

    Functions: Wacky
    Added Comments

--- Functions_Wacky/JS/script.js
+++ Functions_Wacky/JS/script.js
@@ -6,3 +6,7 @@
  * To change this template use File | Settings | File Templates.
  */
 
+//Carmine Trovato        09/27/2013           Functions: Wacky
+
+/* This problem is Converting Miles To Kilometers */
+/* Title console.log */
